5c7c1488e239619476db3f2981651ec546008853
Pett97 <henriquepeterson138@gmail.com>
2025-04-23 01:35:08 -0300

    edit car web ok

---
+++ web/edit/edit.js
@@ -0,0 +1,54 @@
+const URL_BASE = 'http://localhost:3000/carro';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const inputId = document.getElementById('id_carro');
+  const inputMarca = document.getElementById('marca');
+  const inputModelo = document.getElementById('modelo');
+  const inputHp = document.getElementById('hp');
+
+  document.getElementById('btn_atualizar').addEventListener('click', atualizarCarro);
+
+  document.getElementById("btn_voltar").addEventListener('click',function (){
+   window.location.href = '../index.html';
+  })
+
+  async function getCarro() {
+    let idCarro = localStorage.getItem('idCarroEditar');
+    if (!idCarro) {
+      alert('não foi possivel buscar o ID carro');
+    }
+    try {
+      const carro = await axios.get(`${URL_BASE}/${idCarro}`);
+
+      if (!carro) {
+        alert('erro ao buscar o carro');
+      }
+
+      inputId.value = carro.data.id;
+      inputMarca.value = carro.data.marca;
+      inputModelo.value = carro.data.modelo;
+      inputHp.value = carro.data.hp;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function atualizarCarro() {
+    let idCarro = localStorage.getItem('idCarroEditar');
+    let carro = {
+      marca: inputMarca.value,
+      modelo: inputModelo.value,
+      hp: inputHp.value,
+    };
+    try {
+      const response = await axios.patch(`${URL_BASE}/${idCarro}`, carro);
+      alert('Carro atualizado com sucesso!');
+      window.location.href = '../index.html';
+    } catch (error) {
+      console.error('Erro ao atualizar o carro:', error);
+      alert('Erro ao atualizar o carro');
+    }
+  }
+
+  getCarro();
+});
--- web/index.js
+++ web/index.js
@@ -1,14 +1,72 @@
-const URL_BASE = "http://localhost:3000/carro/";
+const URL_BASE = 'http://localhost:3000/carro';
 
 document.addEventListener('DOMContentLoaded', function () {
+  localStorage.clear();
+
   async function getCarros() {
     try {
       const response = await axios.get(URL_BASE);
-      console.log("Carros:", response.data);
+      return response.data;
     } catch (error) {
-      console.error("Erro ao buscar os carros:", error);
+      console.error('Erro ao buscar os carros:', error);
     }
   }
 
+  async function montarTabelaCarros() {
+    let carros = await getCarros();
+    if (!carros) {
+      alert('não foi possivel buscar os carros');
+      return;
+    }
+
+    const corpoTabela = carros
+      .map(
+        (carro) => `
+      <tr>
+        <td>${carro.id}</td>
+        <td>${carro.marca}</td>
+        <td>${carro.modelo}</td>
+        <td>${carro.hp}</td>
+        <td>
+          <div id="acoes">
+            <button onclick="editarCarro(event)" data-id="${carro.id}">Editar</button>
+            <button onclick="deletarCarro(event)" data-id="${carro.id}">Deletar</button>
+          </div>
+        </td>
+      </tr>
+    `,
+      )
+      .join('');
+
+    document.getElementById('tabela-carros').innerHTML = corpoTabela;
+  }
+
+  window.editarCarro = function (event) {
+    let idCarro = event.target.getAttribute('data-id');
+    localStorage.setItem('idCarroEditar', idCarro);
+    if (localStorage.getItem('idCarroEditar')) {
+      window.location.href = './edit/edit.html';
+    }
+  };
+
+  window.deletarCarro = async function (event) {
+    let idCarro = event.target.getAttribute('data-id');
+    if (!idCarro) {
+      alert('não foi possivel buscar o id carro');
+    }
+    try {
+      const response = await axios.delete(`${URL_BASE}/${idCarro}`);
+      if (response.status === 200 || response.status === 204) {
+        alert('Carro deletado com sucesso');
+        window.location.reload();
+      } else {
+        alert('Não foi possível deletar o carro');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   getCarros();
+  montarTabelaCarros();
 });
